eed6b59feea13881d0e0b77679c50ad2777b3aee
tateana2002 <uona2002@yandex.ru>
2016-08-01 17:51:56 +0300

    Домашняя работа ,AddNewContact

---
+++ addressbook-web-tests/src/test/java/il/stq/pft/addressbook/AddForm.java
@@ -0,0 +1,37 @@
+package il.stq.pft.addressbook;
+
+public class AddForm {
+  private final String name;
+  private final String lastname;
+  private final String address;
+  private final String telefon;
+  private final String emailaddress;
+
+  public AddForm(String name, String lastname, String address, String telefon, String emailaddress) {
+    this.name = name;
+    this.lastname = lastname;
+    this.address = address;
+    this.telefon = telefon;
+    this.emailaddress = emailaddress;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getTelefon() {
+    return telefon;
+  }
+
+  public String getEmailaddress() {
+    return emailaddress;
+  }
+}
