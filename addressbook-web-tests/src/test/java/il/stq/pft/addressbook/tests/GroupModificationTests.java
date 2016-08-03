927501fdb0757301dad2a7e229c2c0c6020ffa3d
tateana2002 <uona2002@yandex.ru>
2016-08-03 04:49:21 +0300

    Создан класс модификатор для групп

---
+++ addressbook-web-tests/src/test/java/il/stq/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,25 @@
+package il.stq.pft.addressbook.tests;
+
+import il.stq.pft.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+/**
+ * Created by vovka on 03.08.2016.
+ */
+public class GroupModificationTests extends TestBase{
+  @Test
+
+  public void testGroupModification(){
+    app.getNavigationHelper().goToGroupPage();
+   // app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test11", "test2w", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGoPage();
+
+  }
+
+
+
+
+}
